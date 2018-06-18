3c9a1af8581a2a10900351d5922726c9705ddd1e
Rahul Thakare <rahulthakare1504@gmail.com>
2018-06-18 18:54:37 +0530

    Added settings file

--- InsertDateTime.py
+++ InsertDateTime.py
@@ -3,6 +3,14 @@ import sublime_plugin
 import time
 
 class InsertDateTimeCommand(sublime_plugin.TextCommand):
+
+    DEFAULT_DATE_FORMAT = "%d-%m-%Y"
+    DEFAULT_TIME_FORMAT = "%I:%M:%S"
+    DEFAULT_DATETIME_FORMAT = "%d-%m-%Y %I:%M:%S"
+    DEFAULT_PLAIN_DATE_FORMAT = "%d%m%Y"
+    DEFAULT_PLAIN_TIME_FORMAT = "%I%M%S"
+    DEFAULT_PLAIN_DATETIME_FORMAT = "%d%m%Y %I%M%S"
+
     def run(self, edit, **kwargs):
         self.edit = edit
         command = kwargs.get('command', None)
@@ -21,25 +29,39 @@ class InsertDateTimeCommand(sublime_plugin.TextCommand):
             self.insert_plain_datetime()
 
     def insert_date(self):
-        self.put_in("%d-%m-%Y")
+        self.put_in(
+            self.get_font_settings('date_format', self.DEFAULT_DATE_FORMAT)
+        )
 
     def insert_time(self):
-        self.put_in("%I:%M:%S")
+        self.put_in(
+            self.get_font_settings('time_format', self.DEFAULT_TIME_FORMAT)
+        )
 
     def insert_datetime(self):
-        self.put_in("%d-%m-%Y %I:%M:%S")
+        self.put_in(
+            self.get_font_settings('datetime_format', self.DEFAULT_DATETIME_FORMAT)
+        )
 
     def insert_plain_date(self):
-        self.put_in("%d%m%Y")
+        self.put_in(
+            self.get_font_settings('plain_date_format', self.DEFAULT_PLAIN_DATE_FORMAT)
+        )
 
     def insert_plain_time(self):
-        self.put_in("%I%M%S")
+        self.put_in(
+            self.get_font_settings('plain_time_format', self.DEFAULT_PLAIN_TIME_FORMAT)
+        )
 
     def insert_plain_datetime(self):
-        self.put_in("%d%m%Y %I%M%S")
-
+        self.put_in(
+            self.get_font_settings('plain_datetime_format', self.DEFAULT_PLAIN_DATETIME_FORMAT)
+        )
 
     def put_in(self, format):
         sel = self.view.sel();
         for s in sel:
             self.view.replace(self.edit, s, time.strftime(format))
+
+    def get_font_settings(self, setting, default):
+        return sublime.load_settings('InsertDateTime.sublime-settings').get(setting, default)
